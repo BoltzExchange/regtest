79d8c6515a92bd12263b11d29b807bb984639ec7
maybeast <78227110+maybeast@users.noreply.github.com>
2024-06-01 14:58:15 +0300

    copy credentials for host; expose boltz grpc to host

--- images/scripts/entrypoint.sh
+++ images/scripts/entrypoint.sh
@@ -7,6 +7,11 @@ cleanup() {
 }
 
 cp /root/.bitcoin/regtest/.cookie /root/.host-data/.bitcoind-cookie
+cp /root/.lnd-2/tls.cert /root/.host-data/lnd-2-tls.cert
+cp /root/.lnd-2/data/chain/bitcoin/regtest/admin.macaroon /root/.host-data/lnd-2-admin.macaroon
+cp /root/.lightning-1/regtest/ca.pem /root/.host-data/lightning-1-ca.pem
+cp /root/.lightning-1/regtest/client-key.pem /root/.host-data/lightning-1-client-key.pem
+cp /root/.lightning-1/regtest/client.pem /root/.host-data/lightning-1-client.pem
 # TODO: copy over everything we need for the host
 
 trap cleanup SIGINT SIGTERM
