7aea038da50ccd00497d9b13e2b50bcaef4267f9
jackstar12 <62219658+jackstar12@users.noreply.github.com>
2024-10-04 18:58:52 +0200

    feat: mine-block alias (#41)

--- aliases.sh
+++ aliases.sh
@@ -9,6 +9,7 @@ alias bitcoin-cli-sim-server='run_in_container bitcoin-cli-sim-server'
 alias elements-cli-sim-client='run_in_container elements-cli-sim-client'
 alias elements-cli-sim-server='run_in_container elements-cli-sim-server'
 alias boltzcli-sim='run_in_container boltzcli-sim'
+alias mine-block='bitcoin-cli-sim-client -generate 1 && elements-cli-sim-client -generate 1'
 
 lightning-cli-sim() {
     run_in_container lightning-cli-sim "$@"
