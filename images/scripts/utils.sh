1d1d08c6c1e0466185b1ff50870922fa60263cb1
michael1011 <me@michael1011.at>
2025-09-18 21:13:45 +0200

    fix: backend startup

--- images/scripts/utils.sh
+++ images/scripts/utils.sh
@@ -308,7 +308,7 @@ wait-for-cln-sync(){
 
 # Deploy contracts
 deploy_contract() {
-  cast send --rpc-url http://anvil:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --create $1
+  cast send --rpc-url http://anvil:8545 --private-key 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d --create $1
 }
 
 deploy_contracts() {
